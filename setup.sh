e1da19cf6c47f0cb043763a03251d7f1125e1835
Salem Elrahal <selrahal@redhat.com>
2022-07-27 09:20:27 +0100

    moved xsd list into src

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@
 
 # grep "Download XSD" ISO\ 20022\ Message\ Definitions\ \|\ ISO20022.html | cut -d = -f 3 | cut -f 1 -d " " | sed -e "s/\"//g" | sed "/^$/d" > ~/Projects/iso20022/code-with-quarkus/iso20022.txt
 
-URLS=$(cat iso20022.txt)
+URLS=$(cat src/main/resources/iso20022.txt)
 for url in $URLS; do
   wget -c --content-disposition --directory-prefix=src/main/resources/xsd $url 
 done
